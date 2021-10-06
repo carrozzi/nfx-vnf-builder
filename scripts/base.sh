edb6c653ed5a0793bfae65bac34018c99e1579c3
Todd Carrozzi <tcarrozzi@juniper.net>
2021-10-06 17:56:48 -0400

    added dpotts netplan fix

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,4 +6,8 @@ apt-get install -y wget vim htop
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release software-properties-common tshark jq
 systemctl enable serial-getty@ttyS0.service
 systemctl start serial-getty@ttyS0.service
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+systemctl restart sshd
+mv /tmp/01-netcfg.yaml /etc/netplan
+netplan apply
 
